77b3cc722ab699cd39a324b257dbcc3e81f9964c
Sam <ssiegel2@umbc.edu>
2017-10-07 18:33:40 -0400

    Added/Building setup page template

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-// initialize jquery
-var jsdom = require('jsdom').jsdom;
-jsdom.env('', function (err, window) {
-    if(err) {
-        console.error(err);
-        return;
-    }
-    global.$ = require('jquery')(window);
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var mustache = require('mustache');
+var path = require('path')
+
+app.use(express.static(__dirname + '/public'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'))
+
+app.get('/', function(req, res) {
+    fs.readFile('index.html', function(err, template) {
+        res.set('Content-Type', 'text/html');
+        template = template.toString();
+        res.send(mustache.to_html(template, test_data))
+    })
 });
+
+app.listen(8000, function() {
+    console.log('Listening on port 8000');
+});
+
+test_data = {
+    title : 'Server Setup',
+    arch : [
+        {'name': 'MEAN Stack'},
+        {'name': 'LAMP Stack'}
+    ],
+    bootstrap_template : [
+        {'name': 'Template 1'},
+        {'name': 'Template 2'}
+    ]
+}
