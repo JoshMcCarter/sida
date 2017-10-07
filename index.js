53371ec3482e1092b11d6bf0fa1f71b619215ee6
Sam <ssiegel2@umbc.edu>
2017-10-07 19:27:57 -0400

    Page template gets data from test object, outputs test output

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 var mustache = require('mustache');
-var path = require('path')
+var path = require('path');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
 
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'))
@@ -13,6 +16,11 @@ app.get('/', function(req, res) {
         template = template.toString();
         res.send(mustache.to_html(template, test_data))
     })
+});
+
+app.post('/config', function (req, res) {
+    console.log(req.body);
+    res.send(req.body);
 });
 
 app.listen(8000, function() {
